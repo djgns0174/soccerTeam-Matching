ce284cab47a55f7691f08e34a9f4480056a42ca9
yehoon <djgns01631221@gmail.com>
2024-07-01 20:21:25 +0900

    asdf

---
+++ back/src/test/java/com/example/soccerTeam_back/SoccerTeamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soccerTeam_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerTeamBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
