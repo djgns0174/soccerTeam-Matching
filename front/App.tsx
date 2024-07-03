94a241208e11cc0176c8d963a0f0f9f0f59425b3
yehoon <djgns01631221@gmail.com>
2024-07-03 17:12:08 +0900

    asdf

--- front/App.tsx
+++ front/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import axios from 'axios';
 import Bar1 from './src/screens/bar1';
 import Bar2 from './src/screens/bar2';
-import Bar3 from './src/screens/bar3';
-import MenuBar from './src/screens/menuBar';
+import Bar3 from './src/screens/team/bar3';
+import MenuBar from './src/screens/team/menuBar';
 import TeamLeague from './src/screens/team/teamLeague';
 import MatchResult from './src/screens/team/matchResult';
 import TeamRanking from './src/screens/team/teamRanking';
@@ -35,10 +35,10 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <Bar1/>
-      <Bar2/>
-      <MenuBar/>
-      <Bar3 />
+      <Bar1 />
+      <Bar2 />
+      <MenuBar />
+      <Bar3 setSelectedComponent={setSelectedComponent} selectedComponent={selectedComponent} />
       {renderComponent()}
     </View>
   );
---
+++ front/src/screens/team/bar3.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+const Bar3 = ({ selectedComponent, setSelectedComponent }) => {
+  console.log(selectedComponent);
+  return (
+    <View style={styles.scheduleHeader}>
+      <TouchableOpacity onPress={() => setSelectedComponent('teamLeague')}>
+        <Text style={[
+          styles.scheduleTitle,
+          selectedComponent === 'teamLeague' ? styles.selectedText : null
+        ]}>일정</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setSelectedComponent('matchResult')}>
+        <Text style={[
+          styles.scheduleTitle,
+          selectedComponent === 'matchResult' ? styles.selectedText : null
+        ]}>경기 결과</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setSelectedComponent('teamRanking')}>
+        <Text style={[
+          styles.scheduleTitle,
+          selectedComponent === 'teamRanking' ? styles.selectedText : null
+        ]}>팀 순위</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  scheduleHeader: {
+    width: '88%',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    paddingBottom: 10,
+  },
+  scheduleTitle: {
+    marginRight: 15,
+    fontSize: 12,
+  },
+  selectedText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+});
+
+export default Bar3;
